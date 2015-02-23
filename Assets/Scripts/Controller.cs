46eb777bff02991bad30e2678dfaa8ef2d81c263
Joshua Wardlaw <jwardlaw@uci.edu>
2015-02-22 17:58:35 -0800

    setup

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	float hspeed = 1f;
+	float vspeed = 1f;
+	float direction = 0f;
+
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( Input.GetAxis ("Vertical") > 0 || Input.GetAxis("Vertical") < 0 )
+		{
+			direction = Input.GetAxis ("Vertical");
+		}
+		gameObject.transform.position = new Vector2(transform.position.x + hspeed, transform.position.y + vspeed*direction);
+	
+	}
+}
